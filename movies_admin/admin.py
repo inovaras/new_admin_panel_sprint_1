067933727c0951eadc4f258df562fad8a8e80b65
Albina Gilmutdinova <inovaaras@yandex.ru>
2024-07-15 16:26:05 +0500

    готова вторая часть задачи

---
+++ movies_admin/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from .models import *
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created', 'modified',)
+    list_filter = ('name',)
+    search_fields = ('name', 'id',)
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('full_name',)
+    search_fields = ('full_name',)
+
+
+class GenreFilmworkInline(admin.TabularInline):
+    model = GenreFilmwork
+
+
+class PersonFilmworkInline(admin.TabularInline):
+    model = PersonFilmwork
+
+
+@admin.register(Filmwork)
+class FilmworkAdmin(admin.ModelAdmin):
+    inlines = (GenreFilmworkInline, PersonFilmworkInline,)
+
+    list_display = ('title', 'type', 'creation_date', 'rating',)
+    list_filter = ('type', 'rating',)
+    search_fields = ('title', 'description', 'id', 'genre')
