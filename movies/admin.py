ba152cd1217c9191247e0a51a6693150ad5515f1
Albina Gilmutdinova <inovaaras@yandex.ru>
2024-07-15 11:27:43 +0500

    add filmwork app

--- movies/admin.py
+++ movies/admin.py
@@ -5,3 +5,8 @@ from .models import *
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(Filmwork)
+class FilmworkAdmin(admin.ModelAdmin):
+    pass
--- movies/models.py
+++ movies/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.db import models
 
+
 class Genre(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField('name', max_length=255)
@@ -12,3 +13,19 @@ class Genre(models.Model):
         db_table = "content\".\"genre"
         verbose_name = 'Жанр'
         verbose_name_plural = 'Жанры'
+
+
+class Filmwork(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField('name', max_length=255)
+    description = models.TextField('description', blank=True)
+    creation_date = models.DateField()
+    rating = models.DecimalField('rating', max_digits=10, decimal_places=1)
+    type = models.CharField('type', max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "content\".\"film_work"
+        verbose_name = 'Кинопроизведение'
+        verbose_name_plural = 'Кинопроизведения'
